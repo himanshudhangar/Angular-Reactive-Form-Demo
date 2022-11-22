ac19156c130040964ba0aec7b0e6a227faccb398
Git with himanshu <dhangarhimanshu450@gmail.com>
2022-11-22 22:49:29 +0530

    Added:Angular Reactive form

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {FormControl,FormControlName, FormGroup} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Reactive Form';
+  loginForm= new FormGroup ({
+    user:new FormControl(' '),
+    password:new FormControl('')
+  })
+
+  loginUser()
+  {
+    console.warn(this.loginForm);
+
+  }
+}
